f221edad61ea3c7866d40caacd36524e6b5e9d7d
seraimu <39331589+seraimu@users.noreply.github.com>
2020-07-30 21:02:15 +0900

    Support PWA

---
+++ js/main.js
@@ -0,0 +1,13 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("js/service_worker.js")
+        .then((registration) => {
+            console.log(
+                "ServiceWorker registration successful with scope: ",
+                registration.scope
+            );
+        })
+        .catch((err) => {
+            console.log("ServiceWorker registration failed: ", err);
+        });
+}
