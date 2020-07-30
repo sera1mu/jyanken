212bf7324d29d740fc26d802e25a0a4ffd45b84e
seraimu <seraimu.neko@outlook.jp>
2020-07-30 20:38:35 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,62 +1,21 @@
 const count = { all: 0, win: 0, draw: 0, lose: 0 };
+const selects = { 0: "rock", 1: "scissors", 2: "paper" };
+
+const updateImage = (imgName, element) => {
+    element[0].children[0].children[0].setAttribute(
+        "srcset",
+        `img/${imgName}.webp`
+    );
+    element[0].children[0].children[1].setAttribute(
+        "src",
+        `img/${imgName}/png`
+    );
+    return;
+};
 
 const render = (imgName, className) => {
-    var elem = document.getElementsByClassName(className);
-    if (className === "result") {
-        switch (imgName) {
-            case "勝ち":
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/win.webp"><img src="img/win.png"></picture><p>Result</p>';
-                break;
-            case "あいこ":
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/draw.webp"><img src="img/draw.png"></picture><p>Result</p>';
-                break;
-            case "負け":
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/lose.webp"><img src="img/lose.png"></picture><p>Result</p>';
-                break;
-            default:
-                console.error("Undefined image name.");
-                break;
-        }
-    } else if (className === "pcSelect") {
-        switch (imgName) {
-            case 0:
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/rock.webp"><img src="img/rock.png"></picture><p>You</p>';
-                break;
-            case 1:
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/scissors.webp"><img src="img/scissors.png"></picture><p>You</p>';
-                break;
-            case 2:
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/paper.webp"><img src="img/paper.png"></picture><p>You</p>';
-                break;
-            default:
-                console.error("Undefined image name.");
-                break;
-        }
-    } else {
-        switch (imgName) {
-            case 0:
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/rock.webp"><img src="img/rock.png"></picture><p>NPC</p>';
-                break;
-            case 1:
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/scissors.webp"><img src="img/scissors.png"></picture><p>NPC</p>';
-                break;
-            case 2:
-                elem[0].innerHTML =
-                    '<picture><source type="image/webp" srcset="img/paper.webp"><img src="img/paper.png"></picture><p>NPC</p>';
-                break;
-            default:
-                console.error("Undefined image name.");
-                break;
-        }
-    }
+    let elem = document.getElementsByClassName(className);
+    updateImage(imgName, elem);
     return;
 };
 
@@ -64,20 +23,20 @@ const judge = (pc, npc) => {
     let result = (pc - npc + 3) % 3;
     switch (result) {
         case 0:
-            return "あいこ";
+            return "draw";
         case 2:
-            return "勝ち";
+            return "win";
         default:
-            return "負け";
+            return "lose";
     }
 };
 
 const addCount = (type) => {
     let elem = document.getElementsByClassName("count");
     count.all++;
-    if (type === "勝ち") {
+    if (type === "win") {
         count.win++;
-    } else if (type === "負け") {
+    } else if (type === "lose") {
         count.lose++;
     } else {
         count.draw++;
@@ -87,8 +46,8 @@ const addCount = (type) => {
 };
 
 const viewResult = (pcSelect, npcSelect, result) => {
-    render(pcSelect, "pcSelect");
-    render(npcSelect, "npcSelect");
+    render(selects[pcSelect], "pcSelect");
+    render(selects[npcSelect], "npcSelect");
     render(result, "result");
     return;
 };
