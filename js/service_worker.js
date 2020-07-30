49a5d1b9c92d2f3eb877afdcd0d3b11d0be5554b
seraimu <39331589+seraimu@users.noreply.github.com>
2020-07-30 21:06:33 +0900

    Update service_worker.js

--- js/service_worker.js
+++ js/service_worker.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 var CACHE_NAME = "rsp-caches";
 var urlsToCache = [
-    "/seraimu.github.io/jyanken/index.html",
-    "/seraimu.github.io/jyanken/game.html",
-    "/seraimu.github.io/jyanken/changelog.html",
-    "/seraimu.github.io/jyanken/img/",
-    "/seraimu.github.io/jyanken/js/",
-    "/seraimu.github.io/jyanken/css/",
-    "/seraimu.github.io/jyanken/vendor/",
-    "/seraimu.github.io/jyanken/manifest.json",
-    "/seraimu.github.io/jyanken/LICENSE",
+    "/jyanken/index.html",
+    "/jyanken/game.html",
+    "/jyanken/changelog.html",
+    "/jyanken/img/",
+    "/jyanken/js/",
+    "/jyanken/css/",
+    "/jyanken/vendor/",
+    "/jyanken/manifest.json",
+    "/jyanken/LICENSE",
 ];
 
 self.addEventListener("install", (event) => {
